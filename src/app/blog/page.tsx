c760d016bbf45a0ac7a9fb8041cdc6b56f91d220
Phi <dungho01092002@gmail.com>
2023-03-03 02:51:27 +0800

    incremental update 1

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,13 @@
 import { SectionContainer } from '~/components';
 
 export default function Blog() {
-  return <SectionContainer className="h-screen flex flex-col items-center justify-center"></SectionContainer>;
+  return (
+    <SectionContainer className="h-screen flex flex-col items-center justify-center">
+      <img
+        src="https://readme-typing-svg.herokuapp.com?
+        font=Fira+Code&weight=500&size=24&duration=4000&pause=1000&color=F75B00&center=true&vCenter=true&multiline=true&width=900&lines=Blogs+are+being+migrated.+Please%2C+comeback+soon."
+        alt="Typing SVG"
+      />
+    </SectionContainer>
+  );
 }
