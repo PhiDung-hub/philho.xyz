aaab1dab901d92cb93da7d46ad04e8b29da38eb4
Phi <dungho01092002@gmail.com>
2023-05-05 11:20:58 +0800

    remove unist

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,3 @@
 export { default as formateDate } from './formatDate';
 export { default as clsxTailwindMerge } from './clsx_tailwind_merge';
-// export { default as remarkImgToJSX } from './remark_img_to_jsx';
 export { default as rehypePrettyCodeOptions } from './rehype_pretty_code_options';
