98e8c13eb40bddf9b76dc5ba149a25242ccc0665
Phi <dungho01092002@gmail.com>
2023-05-06 20:46:53 +0800

    fix layout

--- src/layouts/blog_page/About.tsx
+++ src/layouts/blog_page/About.tsx
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <div id="about-blogs" className="my-8 md:my-12 font-mono italic text-4xl text-yellow-500 dark:text-blue-200">
+    <div id="about-blogs" className="mb-8 md:mb-12 font-mono italic text-4xl text-yellow-500 dark:text-blue-200">
       My opinions, knowledges, and discoveries
     </div>
   );
