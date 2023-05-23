259870abc7045eedf9f1304bcb51effc057c317b
Phi <dungho01092002@gmail.com>
2023-05-23 14:08:04 +0800

    adjust quote color theme

--- src/app/blog/[...slug]/loading.tsx
+++ src/app/blog/[...slug]/loading.tsx
@@ -3,7 +3,7 @@ import { SectionContainer } from '~/components';
 export default function Projects() {
   return (
     <SectionContainer className="h-screen font-mono text-3xl md:text-4xl animate-text-bounce flex flex-col items-center justify-center">
-      Loading ...
+      Fetching blog data ...
     </SectionContainer>
   );
 }
--- src/components/mdx/Quote.tsx
+++ src/components/mdx/Quote.tsx
@@ -9,10 +9,12 @@ type QuoteProps = {
 
 const Quote = (props: QuoteProps) => {
   return (
-    <div className={clsxTailwindMerge('border-l-[3px] border-current pl-4', props.className)}>
-      <div aria-label="quote-content">{props.children}</div>
+    <div className={clsxTailwindMerge('border-l-[3px] border-opacity-80 border-gray-400 dark:border-gray-700 pl-4', props.className)}>
+      <div aria-label="quote-content" className="opacity-80">
+        {props.children}
+      </div>
       {props.author && (
-        <div aria-label="quote-author" className="italic font-medium text-lg">
+        <div aria-label="quote-author" className="opacity-90 italic font-medium text-lg">
           {props.author}
         </div>
       )}
